b2cb9023d87e35d08d53e8acfdd1aac808e1c84c
yugq <yugq@fenqi.im>
2016-09-07 18:12:07 +0800

    test runload.sh

--- runload.sh
+++ runload.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+LOADNAME=$1;
+
 if [ -z "$LOADNAME" ]; then
   LOADNAME=hello-baidu
 fi
 
 echo "start $LOADNAME.jmx ....";
-/opt/jmeter/bin/jmeter -n -t /jmeter/$LOADNAME.jmx -l /tmp/result_$LOADNAME.jtl;
+bin/jmeter -n -t /jmeter/$LOADNAME.jmx -l /tmp/result_$LOADNAME.jtl;
 echo "end $LOADNAME.jmx ....";
 exit;
